1af00161271087d2b27791a4ecf9ee9dcaaa0324
gen-nakamura <gen0401@keio.jp>
2024-01-06 18:39:58 +0900

    first commit

---
+++ bag_to_npz.py
@@ -0,0 +1,175 @@
+import os
+os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
+# import cv2
+import numpy as np
+import glob
+# import re
+import rosbag
+import matplotlib.pyplot as plt
+
+rosbag_files = glob.glob("./Dataset/rosbag/*.bag")
+srcpath = glob.glob("./Dataset/src/*.txt")[0]
+TOPIC_EVENTS = '/cam0/events'
+SIZE_X = 640
+SIZE_Y = 480
+num_bins = [30, 50, 150]
+
+def events_to_voxel_grid(events, num_bins, width, height):
+    """
+    Build a voxel grid with bilinear interpolation in the time domain from a set of events.
+
+    :param events: a [N x 4] NumPy array containing one event per row in the form: [timestamp, x, y, polarity]
+    :param num_bins: number of bins in the temporal axis of the voxel grid
+    :param width, height: dimensions of the voxel grid
+    """
+
+    assert(events.shape[1] == 4)
+    assert(num_bins > 0)
+    assert(width > 0)
+    assert(height > 0)
+
+    voxel_grid = np.zeros((num_bins, height, width), np.float32).ravel()
+    # イベントデータをタイムスタンプでソートする
+    events = events[events[:, 0].argsort()]
+    timestamps = events[:, 0]
+
+    # normalize the event timestamps so that they lie between 0 and num_bins
+    last_stamp = events[-1, 0]
+    first_stamp = events[0, 0]
+    deltaT = last_stamp - first_stamp
+
+    if deltaT == 0:
+        deltaT = 1.0
+
+
+    events[:, 0] = (num_bins - 1) * (events[:, 0] - first_stamp) / deltaT
+    ts = events[:, 0]
+    xs = events[:, 1].astype(np.int64)
+    ys = events[:, 2].astype(np.int64)
+    # xs = events[:, 1].astype(np.float32)
+    # ys = events[:, 2].astype(np.float32)
+    pols = events[:, 3]
+    pols[pols == 0] = -1  # polarity should be +1 / -1
+
+    tis = ts.astype(np.int64)
+    dts = ts - tis
+    vals_left = pols * (1.0 - dts)
+    vals_right = pols * dts
+
+    valid_indices = tis < num_bins
+    np.add.at(voxel_grid, xs[valid_indices] + ys[valid_indices] * width
+              + tis[valid_indices] * width * height, vals_left[valid_indices])
+
+    valid_indices = (tis + 1) < num_bins
+    np.add.at(voxel_grid, xs[valid_indices] + ys[valid_indices] * width
+              + (tis[valid_indices] + 1) * width * height, vals_right[valid_indices])
+
+    voxel_grid = np.reshape(voxel_grid, (num_bins, height, width))
+
+    return voxel_grid
+
+def load_bag_file(path_to_bag_file):
+    """Bagファイルからのイベントデータの読み込み.
+
+    Args:
+        path_to_bag_file (str): Bagファイルへのパス.
+        timestamp_range (list[int]): [読み込みを始めるタイムスタンプ, 読み込みを終わるタイムスタンプ]の形式のリスト.
+
+    Raises:
+        Exceptions.FileNotExistError: Bagファイルが存在しない場合に発生.
+
+    Returns:
+        np.ndarray: イベントデータ.
+    """
+    
+    print('Loading events...')
+    
+    bag = rosbag.Bag(path_to_bag_file)
+
+    event_list = list()
+    num_events = 0
+    
+    for topic, msg, t in bag.read_messages():
+        if topic == TOPIC_EVENTS:
+            new_event_list = msg.events
+            event_list.extend(new_event_list)
+                
+            num_events += len(new_event_list)
+            print('\r%d events are loaded.' % (num_events), end='')
+    
+    print()
+    
+    events = list()
+    
+    for event in event_list:
+        x = event.x
+        y = event.y
+        timestamp = event.ts.nsecs
+        polarity = 1 if event.polarity else -1
+        #event_array = np.zeros((SIZE_X, SIZE_Y))
+        #event_array[x][y] = polarity
+        event_array = [timestamp, x, y, polarity]
+        events.append(event_array)
+    
+    events = np.array(events, dtype=np.float32)
+
+    return events
+
+def min_max(x):
+    x_min = x.min(axis=None, keepdims=True)
+    x_max = x.max(axis=None, keepdims=True)
+    return (x - x_min) / (x_max - x_min)
+
+
+def get_auto_keys():
+    global f
+    DataName = f.readline().strip()
+    print("DataName = ", DataName)
+    if not DataName:
+        print("EOL")
+        f.close()
+        return
+    CameraVar = list(map(float, f.readline().split(",")))
+    CameraPosition = CameraVar[:3]
+    CameraRotation = CameraVar[3:]
+
+    AutoKey = []
+    KeyNum = int(f.readline())
+    for i in range(KeyNum):
+        AutoKey.append(list(map(float, f.readline().split(","))))
+    
+    LightNum = int(f.readline())
+    for i in range(LightNum):
+        f.readline()
+        # do something with light positions
+
+    # reset_keys(SMPLX_sec)
+    # キーの設定
+    # for key in AutoKey:
+        # set_keys(SMPLX_sec, *key)
+    # set camera position
+    # set light position
+    # set the callback function
+    # generate
+    # res = render_sequence_to_movie_minimal(DataName, PKG_DIR+seq_name, MovieFolderPath+DataName, on_finished_callback)
+    # print('this is response from renderer: ', res)
+    # if finished, remove tr
+    return AutoKey
+
+
+
+if __name__ == "__main__":
+    for num_bin in num_bins:
+        # for rosbag_file in rosbag_files:
+        #     name_input = rosbag_file.replace('rosbag', 'train_data').replace('.bag', f'_{num_bin}.npz')
+        #     # if os.path.exists(name_input):
+        #     #     os.remove(name_input)
+        #     if not os.path.exists(name_input):
+        #         print(f'creating file: {os.path.basename(name_input)}')
+        #         input = load_bag_file(rosbag_file)
+        #         input = events_to_voxel_grid(input, num_bin, SIZE_X, SIZE_Y)
+        #         input = np.array(input, dtype=np.int32)
+        #         input_norm = min_max(input)
+        #         np.savez_compressed(name_input, input_norm)
+
+        f = open(srcpath, "r")
---
+++ train.py
@@ -0,0 +1,197 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+import wandb
+import lpips
+from model import E2VIDRecurrent, E2VID
+#from dataload import load_bag_file
+from torch.utils.data import Dataset, DataLoader
+#from tensorboardX import SummaryWriter
+from datetime import datetime
+#import os
+import matplotlib.pyplot as plt
+#os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
+#import cv2
+import numpy as np
+import glob
+import random
+#import sys
+#sys.path.append("/home/kentahorikawa/E2VID/rpg_e2vid")
+#from utils.inference_utils import events_to_voxel_grid_pytorch, events_to_voxel_grid
+
+input_path = '/home/kentahorikawa/focal_stack/data/processed/synth_rand/Dataset/input_5_norm/'
+target_path = '/home/kentahorikawa/focal_stack/data/processed/synth_rand/Dataset/target_5_norm/'
+config = "/home/kentahorikawa/E2VID/rpg_e2vid/model/config.json"
+SIZE_X = 256
+SIZE_Y = 256
+learning_rate = 0.0001
+epochs = 200
+Loss = "MSE"
+activation = "ReLU"
+num_bin = 5
+batch_size=16
+norm = "BN"
+flip_lr = 0.4
+flip_ud = 0.4
+
+now = datetime.now()
+#log_path = "./log" + now.strftime("%Y%m%d-%H%M%S") + "/"
+#writer = SummaryWriter(log_path)
+plt.figure(figsize=(5, 5))
+plt.title("Loss")
+train_loss = []
+test_loss = []
+
+#device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+class CustomImageDataset(Dataset):
+    def __init__(self, input_dir, target_dir):
+        self.dir_input = input_dir
+        self.dir_target = target_dir
+
+    def __len__(self):
+        return len(glob.glob(self.dir_target + "*"))
+
+    def __getitem__(self, idx):
+        #rd_lr = random.random()
+        #rd_ud = random.random()
+        input_path = self.dir_input + str(idx).zfill(4) + "rosbag.npz"
+        input_loaded = np.load(input_path)
+        input = input_loaded['arr_0']
+        target_path = self.dir_target + str(idx).zfill(4) + "idepth.npy"
+        target = np.load(target_path)
+        print("\rData No." + str(idx).zfill(4) + " was loaded", end="")
+        """
+        if(rd_lr < flip_lr):
+            input = input[:, ::-1, :]
+            target = target[:, ::-1]
+        if(rd_ud < flip_ud):
+            input = input[::-1, :, :]
+            target = target[::-1, :, :]
+        """
+        return input, target
+
+"""
+transform = T.Compose(
+    [T.RandomHorizontalFlip(p=0.5),
+     T.RandomVerticalFlip(p=0.5),
+     T.RandomRotation(degrees=(-20, 20))]
+)
+"""
+
+dataset = CustomImageDataset(input_dir=input_path, target_dir=target_path)
+print(len(dataset))
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [len(dataset) - 50, 50])
+#print(len(train_dataset))
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+print(train_dataloader)
+print(test_dataloader)
+
+data_num = len(train_dataset)
+parameters = "aug_bin" + str(num_bin) \
+    + "_batch" + str(batch_size) \
+        + "_normdata" + str(data_num)  \
+            + "_lr" + str(learning_rate).split('.')[1] \
+                + "_" + activation \
+                    + "_" + Loss \
+                        + "_epoch"+ str(epochs) \
+                            + "_Norm" + str(norm)
+save_model = "/home/kentahorikawa/E2VID/rpg_e2vid/pretrained/model_" + parameters + ".pth"
+
+wandb.init(
+    project = "event-based_DepthEstimation",
+
+    name = parameters,
+
+    config={
+        "train_data_num": data_num,
+        "learning_rate": learning_rate,
+        "activation": activation,
+        "train_loss": Loss,
+        "epochs": epochs,
+        "num_bin": num_bin,
+        "batches": batch_size,
+        "Norm": norm
+    }
+)
+
+def train(dataloader, model, loss_fn, optimizer, epoch_num, batch):
+    model.train()
+    # X: input, y: target
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        #loss = torch.sum(loss)
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    
+    loss_num = loss.item()
+    #writer.add_scalar("Train_Loss", loss_num, epoch_num)
+    wandb.log({"train_loss": loss_num})
+    train_loss.append(loss_num)
+
+
+def test(dataloader, model, loss_fn, epoch_num):
+    model.eval()
+    with torch.no_grad():
+        i=0
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            loss = loss_fn(pred, y)
+            i += 1
+    
+    loss_num = loss.item()
+    #writer.add_scalar("Test_Loss", loss_num, epoch_num)
+    wandb.log({"test_loss": loss_num})
+    test_loss.append(loss_num)
+
+def main():
+    start = datetime.now()
+
+    # Define model
+    model = E2VID(config).to(device)
+    
+    #train_loss_fn = lpips.LPIPS(net='vgg').cuda().forward
+    train_loss_fn = nn.MSELoss()
+    test_loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for t in range(epochs):
+        print(f"[Epoch {t+1}]")
+        train(train_dataloader, model, train_loss_fn, optimizer, t, batch_size)
+        test(test_dataloader, model, test_loss_fn, t)
+        print("\n-------------------------------")
+    end = datetime.now()
+    print("Done!")
+    torch.save(model, save_model)
+    print("Saved PyTorch Model: " + save_model)
+    print("Start: " + start.strftime("%Y%m%d-%H%M%S"))
+    print("End: " + end.strftime("%Y%m%d-%H%M%S"))
+
+    #writer.close()
+    wandb.finish()
+    plt.plot(train_loss, label='train')
+    plt.plot(test_loss, label='test')
+    plt.legend()
+    plt.show()
+    
+
+if __name__ == "__main__":
+    main()
