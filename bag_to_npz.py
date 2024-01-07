399d28b41b2340a23cbb5464913481f1f1d267b6
gen-nakamura <gen0401@keio.jp>
2024-01-07 13:25:44 +0900

    remove comment out

--- bag_to_npz.py
+++ bag_to_npz.py
@@ -187,17 +187,17 @@ if __name__ == "__main__":
     shutil.rmtree('./Dataset/validation_data/')
     os.mkdir('./Dataset/validation_data/')
     for num_bin in num_bins:
-        # for rosbag_file in rosbag_files:
-        #     name_input = rosbag_file.replace('rosbag', 'train_data').replace('.bag', f'_{num_bin}.npz')
-        #     # if os.path.exists(name_input):
-        #     #     os.remove(name_input)
-        #     if not os.path.exists(name_input):
-        #         print(f'creating file: {os.path.basename(name_input)}')
-        #         input = load_bag_file(rosbag_file)
-        #         input = events_to_voxel_grid(input, num_bin, SIZE_X, SIZE_Y)
-        #         input = np.array(input, dtype=np.int32)
-        #         input_norm = min_max(input)
-        #         np.savez_compressed(name_input, input_norm)
+        for rosbag_file in rosbag_files:
+            name_input = rosbag_file.replace('rosbag', 'train_data').replace('.bag', f'_{num_bin}.npz')
+            # if os.path.exists(name_input):
+            #     os.remove(name_input)
+            if not os.path.exists(name_input):
+                print(f'creating file: {os.path.basename(name_input)}')
+                input = load_bag_file(rosbag_file)
+                input = events_to_voxel_grid(input, num_bin, SIZE_X, SIZE_Y)
+                input = np.array(input, dtype=np.int32)
+                input_norm = min_max(input)
+                np.savez_compressed(name_input, input_norm)
 
         f = open(srcpath, "r")
         while True:
